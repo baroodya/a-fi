f7de786135c4d4056fd89a9d38aeb638723063e7
Alexander Baroody <abaroody@yext.com>
2023-03-23 22:22:18 -0400

    rename published model

--- hubconf.py
+++ hubconf.py
@@ -1,13 +1,13 @@
 dependencies = ['torch']
-from architectures import ShallowRegressionLSTM
+from architectures import ShallowRegressionLSTM as _ShallowRegressionLSTM
 
 # resnet18 is the name of entrypoint
-def resnet18(num_features=5, hidden_units=32, **kwargs):
+def ShallowRegressionLSTM(num_features=5, hidden_units=32, **kwargs):
     """ # This docstring shows up in hub.help()
     ShallowLSTM
     num_features: The number of features in the input vector
     hidden_units: the size of the hidden state in the LSTM
     """
     # Call the model, load pretrained weights
-    model = ShallowRegressionLSTM(num_features=num_features, hidden_units=hidden_units, **kwargs)
+    model = _ShallowRegressionLSTM(num_features=num_features, hidden_units=hidden_units, **kwargs)
     return model
