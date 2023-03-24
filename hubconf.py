450a0fe8c2c7a3bc1798c2693ae9b2c331aac2c7
Alexander Baroody <abaroody@yext.com>
2023-03-23 22:10:41 -0400

    add hubconf.py docstring

--- hubconf.py
+++ hubconf.py
@@ -4,8 +4,9 @@ from architectures import ShallowRegressionLSTM
 # resnet18 is the name of entrypoint
 def resnet18(num_features=5, hidden_units=32, **kwargs):
     """ # This docstring shows up in hub.help()
-    Resnet18 model
-    pretrained (bool): kwargs, load pretrained weights into the model
+    ShallowLSTM
+    num_features: The number of features in the input vector
+    hidden_units: the size of the hidden state in the LSTM
     """
     # Call the model, load pretrained weights
     model = ShallowRegressionLSTM(num_features=num_features, hidden_units=hidden_units, **kwargs)
